5058e18c5ef7bc950002dc9e4e7ee2256af09bba
Frank <frank821204@gmail.com>
2015-01-07 16:32:58 +0800

    initial commit

---
+++ quicktooltips.js
@@ -0,0 +1,28 @@
+(function($){
+    $.fn.quickToolTips = function(options) {
+           var tooltips=document.createElement("div");
+           var body=document.body;
+           var $tooltips=$(tooltips);
+           tooltips.className="quicktooltips";
+           body.insertBefore(tooltips,body.firstChild);
+           body.style.position='relative';
+           $tooltips.css({'position':'absolute','display':'none'});
+           return this.each(function() {
+                var defaults={'offsetX':0,'offsetY':0,'displayEvent':'mouseenter','hideEvent':'mouseleave'};
+                options=$.extend({}, defaults, options);
+                var that=this,
+                  $this=$(this);
+                var docLeft,docTop;
+                var message=this.getAttribute('data-tipsmsg')||' ';
+                $this.on(options.displayEvent,function(){
+                    docLeft=$this.offset().left+options.offsetX+'px';
+                    docTop=$this.offset().top+options.offsetY+'px';
+                    $tooltips.css({'top':docTop,'left':docLeft}).html(message).show();
+                })
+                $this.on(options.hideEvent,function(){
+                    $tooltips.hide();
+                })
+        }) 
+    }
+})(jQuery)
+
