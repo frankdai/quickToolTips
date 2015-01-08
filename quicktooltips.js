b1b6aa4093d13c4b371f1b16fbdbb4d93db5fc51
Frank <frank821204@gmail.com>
2015-01-08 10:17:55 +0800

    add event function call

--- quicktooltips.js
+++ quicktooltips.js
@@ -1,4 +1,5 @@
-(function($){
+(function($){ 
+    'use strict'
     $.fn.quickToolTips = function(options) {
            var tooltips=document.createElement("div");
            var body=document.body;
@@ -8,7 +9,13 @@
            body.style.position='relative';
            $tooltips.css({'position':'absolute','display':'none'});
            return this.each(function() {
-                var defaults={'offsetX':0,'offsetY':0,'displayEvent':'mouseover','hideEvent':'mouseout'};
+                var defaults={'offsetX':0,
+                              'offsetY':0,
+                              'displayEvent':'mouseover',
+                              'hideEvent':'mouseout',
+                              displayFunction:function(){},
+                              hideFunction:function(){}
+                              };
                 options=$.extend({}, defaults, options);
                 var that=this,
                   $this=$(this);
@@ -18,9 +25,11 @@
                     docLeft=$this.offset().left+options.offsetX+'px';
                     docTop=$this.offset().top+options.offsetY+'px';
                     $tooltips.css({'top':docTop,'left':docLeft}).html(message).show();
+                    options.displayFunction.call(that);
                 })
                 $this.on(options.hideEvent,function(){
                     $tooltips.hide();
+                    options.hideFunction.call(that);
                 })
         }) 
         return this;
